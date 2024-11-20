1605cecfcd546110e0bb956032765bea7c23c179
Andrey Dodonov <Andrey.Dodonov@endress.com>
2024-11-20 15:21:34 +0100

    Chapter 15: adding authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@ import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+ 
  
 const FormSchema = z.object({
   id: z.string(),
@@ -75,7 +78,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         message: 'Missing Fields. Failed to Update Invoice.',
       };
     }
-    
+
     const { customerId, amount, status } = validatedFields.data;
    
     const amountInCents = amount * 100;
@@ -100,7 +103,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
   }
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Anus Sex!');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
     } catch (error) {
@@ -108,4 +110,23 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         return {message: 'Database Error: Failed to Delete Invoice'};
     }
     revalidatePath('/dashboard/invoices');
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
